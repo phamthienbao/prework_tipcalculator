f8630161f57517745cb822989fbb450a1d3fc80f
Bao Pham <phamthienbao@Baos-MacBook-Pro.local>
2015-08-19 19:46:05 +0700

    Initial Commit

---
+++ TipsCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsCalculator
+//
+//  Created by Bao Pham on 8/19/15.
+//  Copyright (c) 2015 Bao Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
